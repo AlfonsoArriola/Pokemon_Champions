5eca87e5881372a00e62301907d521ecb2be2068
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-04-23 17:16:27 -0400

    inital commit

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    // $("#demo").html("Hello, World!");
+
+    $(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
+
+});
+
+
+
+
+
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
+
+
+
+
+
+
+// class Pokemon{
+// 	constructor( hp, attack, defense, abilities ){
+// 		this.hp = hp;
+// 		this.attack = attack;
+// 		this.defense = defense;
+// 		this.abilities = abilities;
+// 	}
+// }
+
