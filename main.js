2ea24d53220c07d2912998aa83271593978ef8e8
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-04-26 16:48:02 -0400

    so much

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
+let tyrunt = [];
+let getTyruntName = [];
+let getTyruntHP = [];
+let getTyruntAttack = [];
+let getTyruntDefense = [];
+let getTyruntAbilities = [];
+
+
+// let pokemonTitle = document.querySelector('#pokemonTitleName');
+
+
+
+// **************************  JS ends/ jQ begins  ***************************************
+
+
 $(document).ready(function() {
 
+
+// ^^^^^^^^^^^^^^^^^^^^^^  I CHOOSE YOU AJAX !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+       $.ajax({
+ 	    type: 'GET',
+        url:'https://pokeapi.co/api/v2/pokemon/696/',
+        success: function(data){ 
+ 		
+ 		 getTyruntName = data.name;
+ 		 getTyruntHP = data.stats[5].base_stat;
+ 		 getTyruntAttack = data.stats[4].base_stat;
+ 		 getTyruntDefense = data.stats[3].base_stat;
+         
+
+           for (let i =0; i < data.abilities.length; i++){
+           	    getTyruntAbilities = data.abilities[i].ability.name;        
+           };
+
+
+              class Pokemon{
+				constructor(p_name, p_hp, p_attack, p_defense, p_abilities){
+					this.name = p_name;
+					this.hp = p_hp;
+					this.attack = p_attack;
+					this.defense = p_defense;
+					this.abilities = p_abilities;
+			    }	
+              };
+
+
+				let iChooseYouTyrunt = new Pokemon(getTyruntName, getTyruntHP, getTyruntAttack, getTyruntDefense, getTyruntAbilities);
+				console.log( iChooseYouTyrunt);
+				 tyrunt = iChooseYouTyrunt;
+
+				
+				 $('#pokemonTitleName').text(tyrunt.name).toUpperCase();
+
+				 // $('#pokemonTitleName').myclass{text-transform: lowercase};
+
+
+	    }
+        });
+
+
+//   -________________________S__L__I__D__E__R____________________________________________________
 
     $(function(){
     $('.fadein img:gt(0)').hide();
@@ -9,6 +68,9 @@ $(document).ready(function() {
          .end().appendTo('.fadein');}, 
       4000);
 });
+
+// _>>>>>>>>>>>>>>>>>>> Menu <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 
     $('.icon-menu').click(function() {
     $('.menu').animate({	
@@ -50,30 +112,23 @@ $(document).ready(function() {
 });
 // ================================     Jquery Ends/ JS Begins   +++++++++++++++++++++++++
 
-// class Pokemon{
+
+
+// class Cash_Money{
 // 	constructor(){
-		
+
 // 	}
 
-// 	namedAll(){
-//        return 
+// 	all(){
+
 // 	}
 
 // 	get(name){
 
 // 	}
-// };
+// }
 
 
 
 
 
-
-
-
-// let cash_money = {
-// 	let tryunt = { 'hp': 100, 'attack':100, 'defense': 100, 'abilities': []},
-//     let throh =  { 'hp': 100, 'attack':100, 'defense': 100, 'abilities': []},
-//     let tryunt2 =  { 'hp': 100, 'attack':100, 'defense': 100, 'abilities': []},
-
-// };
