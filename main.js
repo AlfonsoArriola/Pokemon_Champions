556db617744c99e5985609c9c0075b196bdc9a2c
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-05-01 13:46:44 -0400

    semi-complete

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let getTyruntHP = [];
 let getTyruntAttack = [];
 let getTyruntDefense = [];
 let getTyruntAbilities = [];
+let getTyruntSpeed =[];
+let getTyruntSpecialDefense = [];
+let getTyruntType =[];
 let tyrunt = [];
 
 let littenDNA = [];
@@ -12,6 +15,9 @@ let getLittenHP = [];
 let getLittenAttack = [];
 let getLittenDefense = [];
 let getLittenAbilities = [];
+let getLittenSpeed = [];
+let getLittenSpecialDefense =[];
+let getLittenType =[];
 let litten = [];
 
 let throhDNA = [];
@@ -20,6 +26,9 @@ let getThrohHP = [];
 let getThrohAttack = [];
 let getThrohDefense = [];
 let getThrohAbilities = [];
+let getThrohSpeed =[];
+let getThrohSpecialDefense = [];
+let getThrohType = [];
 let throh=[];
 
 
@@ -100,6 +109,8 @@ $(document).ready(function(){
 							 		 getTyruntHP = data1.stats[5].base_stat;
 							 		 getTyruntAttack = data1.stats[4].base_stat;
 							 		 getTyruntDefense = data1.stats[3].base_stat;
+							 		 getTyruntSpeed = data1.stats[0].base_stat;
+								 	 getTyruntSpecialDefense = data1.stats[1].base_stat;
 
 									         
 									           for (let i =0; i < data1.abilities.length; i++){
@@ -107,16 +118,18 @@ $(document).ready(function(){
 									           };
 
 							                class Pokemon{
-											  constructor(p_name, p_hp, p_attack, p_defense, p_abilities){
+											  constructor(p_name, p_hp, p_attack, p_defense, p_abilities, p_speed, p_specialDefense){
 												this.name = p_name;
 												this.hp = p_hp;
 												this.attack = p_attack;
 												this.defense = p_defense;
 												this.abilities = p_abilities.toString();
+												this.speed = p_speed;
+												this.specialDefense = p_specialDefense;
 										      }	
 							                };
 
-											tyrunt = new Pokemon(getTyruntName, getTyruntHP, getTyruntAttack, getTyruntDefense, getTyruntAbilities);
+											tyrunt = new Pokemon(getTyruntName, getTyruntHP, getTyruntAttack, getTyruntDefense, getTyruntAbilities, getTyruntSpeed, getTyruntSpecialDefense);
 											console.log( tyrunt);
 											// theSquad.push(tyrunt);
 										
@@ -128,11 +141,15 @@ $(document).ready(function(){
 															 $('#tyruntAttackBarNumber').text(tyrunt.attack);
 															 $('#tyruntDefenseBarNumber').text(tyrunt.defense);
 															 $('#tyruntAbilities').text(tyrunt.abilities);
+															 $('#tyruntSpeedBarNumber').text(tyrunt.speed);
+															 $('#tyruntSpecialDefBarNumber').text(tyrunt.specialDefense);
 
 
 													$('.tyruntHPgrahped').css('width', (tyrunt.hp *4));
 													$('.tyruntATKgraphed').css('width', (tyrunt.attack *4));
 													$('.tyruntDEFgraphed').css('width', (tyrunt.defense *4));
+													$('.tyruntSPDgraphed').css('width', (tyrunt.speed * 4));
+													$('.tyruntSDFgraphed').css('width', (tyrunt.specialDefense * 4));
 
 							        }
 							});
@@ -151,6 +168,9 @@ $(document).ready(function(){
 								 		 getLittenHP = data2.stats[5].base_stat;
 								 		 getLittenAttack = data2.stats[4].base_stat;
 								 		 getLittenDefense = data2.stats[3].base_stat;
+								 		 getLittenSpeed = data2.stats[0].base_stat;
+								 		 getLittenSpecialDefense = data2.stats[1].base_stat;
+
 								         
 									           for (let i =0; i < data2.abilities.length; i++){
 									           	    getLittenAbilities.push(" " + data2.abilities[i].ability.name);
@@ -158,17 +178,26 @@ $(document).ready(function(){
 
 									           };
 
+
+									           for (let i=0; i< data2.types.length; i++){
+									           	 getLittenType.push(" " + data2.types[i].type.name);
+									           };
+
 									              class Pokemon{
-													constructor(p_name, p_hp, p_attack, p_defense, p_abilities){
+													constructor(p_name, p_hp, p_attack, p_defense, p_abilities, p_speed, p_specialDefense, p_types){
 														this.name = p_name;
 														this.hp = p_hp;
 														this.attack = p_attack;
 														this.defense = p_defense;
 														this.abilities = p_abilities.toString();
+														this.speed = p_speed;
+														this.specialDefense = p_specialDefense;
+														// this.types = p_types.toString();
+
 												    }	
 									              };
 
-													litten = new Pokemon(getLittenName, getLittenHP, getLittenAttack, getLittenDefense, getLittenAbilities);
+													litten = new Pokemon(getLittenName, getLittenHP, getLittenAttack, getLittenDefense, getLittenAbilities, getLittenSpeed, getLittenSpecialDefense);
 													console.log(litten);
 													// theSquad.push(litten);
 
@@ -177,10 +206,15 @@ $(document).ready(function(){
 															 $('#littenAttackBarNumber').text(litten.attack);
 															 $('#littenDefenseBarNumber').text(litten.defense);
 															 $('#littenAbilities').text(litten.abilities);
+															 $('#littenSpeedBarNumber').text(litten.speed);
+															 $('#littenSpecialDefBarNumber').text(litten.specialDefense);
+															 $('#littenType').text(litten.types);
 
 													$('.littenHPgrahped').css('width', (litten.hp *4));
 													$('.littenATKgraphed').css('width', (litten.attack *4));
 													$('.littenDEFgraphed').css('width', (litten.defense *4));
+													$('.littenSPDgraphed').css('width', (litten.speed *4));
+													$('.littenSDFgraphed').css('width', (litten.specialDefense *4));
 													
 
 
@@ -203,22 +237,26 @@ $(document).ready(function(){
 								 		 getThrohHP = data3.stats[5].base_stat;
 								 		 getThrohAttack = data3.stats[4].base_stat;
 								 		 getThrohDefense = data3.stats[3].base_stat;
+								 		 getThrohSpeed = data3.stats[0].base_stat;
+								 		 getThrohSpecialDefense = data3.stats[1].base_stat;
 								         
 									           for (let i =0; i < data3.abilities.length; i++){
 									           	    getThrohAbilities.push(" " + data3.abilities[i].ability.name);        
 									           };
 
 										              class Pokemon{
-														constructor(p_name, p_hp, p_attack, p_defense, p_abilities){
+														constructor(p_name, p_hp, p_attack, p_defense, p_abilities, p_speed, p_specialDefense){
 															this.name = p_name;
 															this.hp = p_hp;
 															this.attack = p_attack;
 															this.defense = p_defense;
 															this.abilities = p_abilities.toString();
+															this.speed = p_speed;
+															this.specialDefense = p_specialDefense;
 													    }	
 										              };
 
-															throh = new Pokemon(getThrohName, getThrohHP, getThrohAttack, getThrohDefense, getThrohAbilities);
+															throh = new Pokemon(getThrohName, getThrohHP, getThrohAttack, getThrohDefense, getThrohAbilities, getThrohSpeed, getThrohSpecialDefense);
 															console.log(throh);
 															// theSquad.push(throh);
 
@@ -227,10 +265,14 @@ $(document).ready(function(){
 															 $('#throhAttackBarNumber').text(throh.attack);
 															 $('#throhDefenseBarNumber').text(throh.defense);
 															 $('#throhAbilities').text(throh.abilities);
+															 $('#throhSpeedBarNumber').text(throh.speed);
+															 $('#throhSpecialDefBarNumber').text(throh.specialDefense);
 
 													$('.throhHPgrahped').css('width', (throh.hp *4));
 													$('.throhATKgraphed').css('width', (throh.attack *4));
 													$('.throhDEFgraphed').css('width', (throh.defense *4));
+													$('.throhSPDgraphed').css('width', (throh.speed * 4));
+													$('.throhSDFgraphed').css('width', (throh.specialDefense * 4));
 															
 							        }
 							}); 
@@ -267,6 +309,20 @@ $(document).ready(function(){
        //                      IwannnaBeTheVeryBest = new Cash_Money('throh','litten','tyrunt');
 
        //                              console.log(IwannnaBeTheVeryBest.all());
+
+
+
+
+
+
+
+
+													             $('button').click(function(){
+													             	  $('.extra').slideToggle();
+													             	  
+
+																															             	 
+													             });
 
 
                    });
